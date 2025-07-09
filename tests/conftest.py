655e3bc6696bde8d2546130f095c211e49762891
KrisKulbych <k.kulbych@gmail.com>
2025-07-09 16:48:38 +0300

    Splited temp_data_directory into 4 fixtures; Improved tests by asserting response content

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,47 +1,61 @@
+from copy import deepcopy
 from pathlib import Path
 
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient
 
+from formula1_web_app.app_factory import create_app
 from formula1_web_app.settings import settings
-from main import create_app
 
 
 @pytest.fixture(scope="session")
-def prepare_database(tmp_path_factory: pytest.TempPathFactory) -> Path:
+def temp_data_directory(tmp_path_factory: pytest.TempPathFactory) -> Path:
     tmp_path = tmp_path_factory.mktemp("data_root")
     data_dir = tmp_path / "data"
     data_dir.mkdir()
 
-    tmp_abbreviations = data_dir / "abbreviations.txt"
+    return data_dir
+
+
+@pytest.fixture(scope="session")
+def temp_abbreviations_file(temp_data_directory: Path) -> Path:
+    tmp_abbreviations = temp_data_directory / "abbreviations.txt"
     abbreviations_content = (
         "PGS_Pierre Gasly_SCUDERIA TORO ROSSO HONDA\n"
         "KMH_Kevin Magnussen_HAAS FERRARI\n"
         "FAM_Fernando Alonso_MCLAREN RENAULT\n"
     )
     tmp_abbreviations.write_text(abbreviations_content)
-
-    tmp_start_log = data_dir / "start.log"
-    start_log_content = "FAM2018-05-24_12:13:04.512\nKMH2018-05-24_12:02:51.003\nPGS2018-05-24_12:07:23.645\n"
-    tmp_start_log.write_text(start_log_content)
-
-    tmp_end_log = data_dir / "end.log"
-    end_log_content = "FAM2018-05-24_12:14:17.169\nKMH2018-05-24_12:04:04.396\nPGS2018-05-24_12:08:36.586\n"
-    tmp_end_log.write_text(end_log_content)
-
-    return data_dir
+    return tmp_abbreviations
 
 
 @pytest.fixture(scope="session")
-def test_app(prepare_database: Path) -> Flask:
-    settings.base_dir = prepare_database
+def temp_start_log_file(temp_data_directory: Path) -> Path:
+    tmp_start_log = temp_data_directory / "start.log"
+    start_log_content = "FAM2018-05-24_12:13:04.512\nKMH2018-05-24_12:02:51.003\nPGS2018-05-24_12:07:23.645\n"
+    tmp_start_log.write_text(start_log_content)
+    return tmp_start_log
 
+
+@pytest.fixture(scope="session")
+def temp_end_log_file(temp_data_directory: Path) -> Path:
+    tmp_end_log = temp_data_directory / "end.log"
+    end_log_content = "FAM2018-05-24_12:14:17.169\nKMH2018-05-24_12:04:04.396\nPGS2018-05-24_12:08:36.586\n"
+    tmp_end_log.write_text(end_log_content)
+    return tmp_end_log
+
+
+@pytest.fixture(scope="session")
+def test_app(temp_data_directory: Path) -> Flask:
+    test_settings = deepcopy(settings)
+    test_settings.base_dir = temp_data_directory
     flask_app = create_app()
+
     flask_app.config.update(
         {
             "TESTING": True,
-        }
+        },
     )
     return flask_app
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,6 +3,8 @@ from http import HTTPStatus
 
 from flask.testing import FlaskClient
 
+from formula1_web_app.settings import settings
+
 
 class TestRoutes:
     def test_redirect_from_root_to_report_page(self, client: FlaskClient) -> None:
@@ -16,19 +18,45 @@ class TestRoutes:
     def test_report_page_returns_success_status(self, client: FlaskClient) -> None:
         # Given
         response = client.get("/report/", follow_redirects=True)
+        data = response.get_data(as_text=True)
         # Then
         assert response.status_code == HTTPStatus.OK
-        assert "Formula1 Race Report" in response.get_data(as_text=True)
+        assert "<h2>Formula1 Race Report</h2>" in data
+        assert "<th>Driver name</th>" in data
+        assert "<td>Kevin Magnussen</td>" in data
 
     def test_correct_sort_order(self, client: FlaskClient) -> None:
         # Given
         response = client.get("/report/?order=asc")
+        data = response.get_data(as_text=True)
         # Then
         assert response.status_code == HTTPStatus.OK
+        assert "Sorting in ascending order" in data
+        assert data.index("Kevin Magnussen") > data.index("Pierre Gasly")
+        assert data.index("Pierre Gasly") > data.index("Fernando Alonso")
+
+    def test_default_sorting_is_used_when_order_parameter_is_invalid(self, client: FlaskClient) -> None:
+        # Given
+        response = client.get("/report/?order=minus")
+        data = response.get_data(as_text=True)
+        # Then
+        assert response.status_code == HTTPStatus.OK
+        assert "Sorting in descending order" in data
+        assert data.index("Kevin Magnussen") < data.index("Pierre Gasly")
+        assert data.index("Pierre Gasly") < data.index("Fernando Alonso")
+
+    def test_not_found_driver_id_request(self, client: FlaskClient) -> None:
+        # Given
+        client_request = "vvv"
+        response = client.get(f"/report/drivers/{client_request}")
+        expected_error_message = f"No driver with ID '{client_request.upper()}' found in race data."
+        # Then
+        assert response.status_code == HTTPStatus.NOT_FOUND
+        assert expected_error_message in html.unescape(response.get_data(as_text=True))
 
     def test_invalid_driver_id_request(self, client: FlaskClient) -> None:
         # Given
-        client_request = "vvv"
+        client_request = "123"
         response = client.get(f"/report/drivers/{client_request}")
         expected_error_message = (
             f"Identifier '{client_request}' is not recognized. Try to use 3-letter code like 'KRF': "
@@ -42,11 +70,33 @@ class TestRoutes:
     def test_correct_driver_id_request(self, client: FlaskClient) -> None:
         # Given
         response = client.get("/report/drivers/FAM")
+        data = response.get_data(as_text=True)
         # Then
         assert response.status_code == HTTPStatus.OK
+        assert "Fernando Alonso" in data
+        assert "MCLAREN RENAULT" in data
+        assert "1:12.657" in data
 
     def test_drivers_page(self, client: FlaskClient) -> None:
         # Given
         response = client.get("/report/drivers")
+        data = response.get_data(as_text=True)
         # Then
         assert response.status_code == HTTPStatus.OK
+        assert "list of drivers" in data
+        assert "Fernando Alonso" in data
+        assert 'href="/report/drivers/FAM"' in data
+
+    def test_clean_cache_success(self, client: FlaskClient) -> None:
+        # Given
+        response = client.get(f"/admin/clean-cache?token={settings.admin_token}")
+        # Then
+        assert response.status_code == HTTPStatus.OK
+        assert b"Cache has been successfully cleaned." in response.data
+
+    def test_clean_cache_invalid_token(self, client: FlaskClient) -> None:
+        # Given
+        response = client.get("/admin/clean-cache?token=wrong_token")
+        # Then
+        assert response.status_code == HTTPStatus.FORBIDDEN
+        assert b"Access denied: invalid or missing token." in response.data
