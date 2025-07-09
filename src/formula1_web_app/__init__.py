d6be0fd08f52276b27e260a3a1f3628d90449221
KrisKulbych <k.kulbych@gmail.com>
2025-07-09 16:29:11 +0300

    Integrate dbuild_q1_report with Flask cache

--- src/formula1_web_app/__init__.py
+++ src/formula1_web_app/__init__.py
@@ -1,3 +1,4 @@
 from .app_factory import create_app
-from .cache_config import init_cache
+from .cache_config import cache
+from .data_loader import get_race_results
 from .error_handlers import register_error_handlers
