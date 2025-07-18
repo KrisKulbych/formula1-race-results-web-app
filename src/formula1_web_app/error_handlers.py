0d7b366f53030d2b8c249b47cdacbe68cad3aae1
KrisKulbych <k.kulbych@gmail.com>
2025-07-18 09:28:44 +0300

    default to HTTPStatus.NOT_FOUND when error.code is None

--- src/formula1_web_app/error_handlers.py
+++ src/formula1_web_app/error_handlers.py
@@ -7,8 +7,10 @@ from werkzeug.exceptions import HTTPException
 def register_error_handlers(app: Flask) -> None:
     @app.errorhandler(404)
     def page_not_found_error(error: HTTPException) -> tuple[str, int]:
-        message: str = error.description or "No driver found"
-        code: int = error.code or HTTPStatus.NOT_FOUND
+        message = error.description or "No driver found"
+        code = error.code
+        if code is None:
+            code = HTTPStatus.NOT_FOUND
 
         if request.path.startswith("/report/drivers/"):
             return message, code
